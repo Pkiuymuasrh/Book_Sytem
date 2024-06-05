969c0f9fc8e9d388254428228897bdc8807c7784
Pkiuymuasrh <168890628+Pkiuymuasrh@users.noreply.github.com>
2024-06-05 15:00:08 +0530

    my book

---
+++ back/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+export const mongoDBURL = 
+'mongodb+srv://piyushk1060:pPMt93AB7vRMJoB1@book-mern.xss3vkb.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-mern'
